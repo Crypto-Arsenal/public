64490e0835b6fbda8e07882bad078c0bcd061347
Alan Wu <ac2wu@edu.uwaterloo.ca>
2022-08-14 16:14:30 -0700

    fix link

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/developer/get-started/python/hello-world">
             Get Started - 5min ⏱️
           </Link>
         </div>
