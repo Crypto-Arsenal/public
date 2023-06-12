073d6203d01bb91947191d8bf62393683697d093
Alan Wu <ac2wu@edu.uwaterloo.ca>
2023-06-12 13:17:10 +0800

    fix md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Crypto Arsenal',
-  tagline: 'Documentation & Guidence',
+  tagline: 'Documentation & Guides',
   url: 'https://docs.crypto-arsenal.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
