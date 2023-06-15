cc1130aa8d9a78dc3380741841a240446b374c5c
Alan Wu <ac2wu@edu.uwaterloo.ca>
2023-06-15 17:46:23 +0800

    feat: image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,16 @@ module.exports = {
   organizationName: 'Crypto-Arsenal', // Usually your GitHub org/user name.
   projectName: 'public', // Usually your repo name.
   plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 300, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
     async function myPlugin(context, options) {
       return {
         name: "docusaurus-tailwindcss",
